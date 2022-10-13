8bc8df689bba569c8b299d05bb634e288f272224
Jacob Barrie <barriejj01Work@gmail.com>
2022-10-13 14:23:02 +1000

    Merging avatar creator manually last commit

--- Assets/Scripts/ButtonUI.cs
+++ Assets/Scripts/ButtonUI.cs
@@ -2,16 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+// Script to control Button UI. Uses unity scene management to direct players to required scene
 
 public class ButtonUI : MonoBehaviour
 {
+    // Create Serialized fields to hold the correct scene to change to
     [SerializeField] private string toCommonRoom = "Common Room";
     [SerializeField] private string toMeetingRoom = "Meeting Room";
     [SerializeField] private string toCreator = "CharacterCreator";
     [SerializeField] private string toOverworld = "Overworld";
     [SerializeField] private string toWhiteboard = "Whiteboard";
-    [SerializeField] private string toDashboard = "Dashboard";
+    [SerializeField] private string toFace = "FaceCreator";
 
+    // Functions to be used with onClick in Button objects
     public void loginButton()
     {
         SceneManager.LoadScene(toCommonRoom);
@@ -32,8 +35,8 @@ public class ButtonUI : MonoBehaviour
     public void whiteboardButton(){
         SceneManager.LoadScene(toWhiteboard);
     }
-
-    public void dashboardButton(){
-        SceneManager.LoadScene(toDashboard);
+    
+    public void faceCreatorButtton() {
+        SceneManager.LoadScene(toFace);
     }
 }
